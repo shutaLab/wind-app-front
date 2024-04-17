3d965aa67d24c99b7691512758455d7bea6c4797
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-17 18:09:55 +0900

    calendarSelect

--- src/routes/WindCalendar.tsx
+++ src/routes/WindCalendar.tsx
@@ -6,14 +6,21 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 const WindCalendar = () => {
+  const handleDateClick = () => {
+    alert("たっぷしたな");
+    // その他の処理を追加できます
+  };
   return (
     <div>
       <div className="">
         <FullCalendar
           height="60vh"
-          plugins={[dayGridPlugin]}
+          plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
+          selectable={true}
+          select={handleDateClick}
         />
       </div>
       <Footer />
