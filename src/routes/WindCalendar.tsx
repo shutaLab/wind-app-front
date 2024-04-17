5cecb70bfa6e7386639c73df73f50b0eb68ba94a
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-17 19:09:21 +0900

    カレンダーui修正

--- src/routes/WindCalendar.tsx
+++ src/routes/WindCalendar.tsx
@@ -7,22 +7,26 @@ import Footer from "../components/Footer";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import Button from "../components/Button";
 const WindCalendar = () => {
   const handleDateClick = () => {
     alert("たっぷしたな");
     // その他の処理を追加できます
   };
   return (
-    <div>
+    <div className="px-2">
       <div className="">
         <FullCalendar
-          height="60vh"
+          height="65vh"
           plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           selectable={true}
           dateClick={handleDateClick}
         />
       </div>
+      <p className="bg-cream-white">12月15日</p>
+      <p>鎌倉学生選手権</p>
+      <Button className="h-full w-full mt-2" text="予定を追加" type="button" />
       <Footer />
     </div>
   );
