a4e3f48250572c11dcde6a0dcc1198cb59c7c7a9
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-17 15:03:36 +0900

    footer ui修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,23 +12,23 @@ const Footer = () => {
     <div className="fixed bottom-0 left-0 right-0 p-3 shadow">
       <div className="flex justify-between">
         <div className=" flex-col text-center text-gray">
-          <HomeFilled className="text-3xl " />
+          <HomeFilled className="text-2xl " />
           <p>ホーム</p>
         </div>
         <div className=" flex-col text-center text-gray">
-          <CalendarFilled className="text-3xl " />
+          <CalendarFilled className="text-2xl " />
           <p>カレンダー</p>
         </div>
         <div className=" flex-col text-center text-gray">
-          <PlusOutlined className="text-3xl " />
+          <PlusOutlined className="text-2xl " />
           <p>出艇</p>
         </div>
         <div className=" flex-col text-center text-gray">
-          <FormOutlined className="text-3xl " />
+          <FormOutlined className="text-2xl " />
           <p>ノート</p>
         </div>
         <div className=" flex-col text-center text-gray">
-          <UserOutlined className="text-3xl " />
+          <UserOutlined className="text-2xl " />
           <p>マイページ</p>
         </div>
       </div>
