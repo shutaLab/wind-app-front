aed1425c06ebc3846b961e167a2ffa35362fa520
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-18 15:59:34 +0900

    footer修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,23 +14,18 @@ const Footer = () => {
       <div className="flex justify-between">
         <Link to="/home" className=" flex-col text-center text-gray">
           <HomeFilled className="text-xl " />
-          <p>ホーム</p>
         </Link>
         <Link to="/calendar" className=" flex-col text-center text-gray">
           <CalendarFilled className="text-xl " />
-          <p>カレンダー</p>
         </Link>
         <Link to="/departure" className=" flex-col text-center text-gray">
           <PlusOutlined className="text-xl " />
-          <p>出艇</p>
         </Link>
         <Link to="/windNote" className=" flex-col text-center text-gray">
           <FormOutlined className="text-xl " />
-          <p>ノート</p>
         </Link>
         <Link to="/myPage" className=" flex-col text-center text-gray">
           <EllipsisOutlined className="text-xl" />
-          <p>その他</p>
         </Link>
       </div>
     </div>
