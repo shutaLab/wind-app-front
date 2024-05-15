149a87497ff647c88e4515974bcefd17c6803fce
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-05-15 15:34:50 +0900

    fix: departureDrawerにテーブルを挿入

--- src/components/DepartureTable.tsx
+++ src/components/DepartureTable.tsx
@@ -1,14 +1,7 @@
 import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Ranking from "../components/Ranking";
-import {
-  Avatar,
-  BottomNavigation,
-  BottomNavigationAction,
-  Box,
-  Tab,
-  Tabs,
-} from "@mui/material";
+
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { styled } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -23,6 +16,12 @@ import {
   TableRow,
 } from "../@/components/ui/table";
 import Button from "../components/Button";
+import { Tabs, TabsList, TabsTrigger } from "../@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
 
 const DepartureTable = () => {
   const [value, setValue] = useState(0);
@@ -30,91 +29,86 @@ const DepartureTable = () => {
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
+  const resources = [
+    { id: "a", title: "山田脩太" },
+    { id: "b", title: "𩜙平名春人" },
+    { id: "c", title: "Task C" },
+    { id: "d", title: "Task D" },
+    { id: "e", title: "Task E" },
+    { id: "f", title: "Task F" },
+    { id: "g", title: "Task G" },
+  ];
+
+  const events = [
+    {
+      id: "1",
+      resourceId: "a",
+      start: "2023-06-14T09:00:00",
+      end: "2023-06-14T11:25:00",
+      title: "Event 1",
+      backgroundColor: "#8EAAE5",
+    },
+    {
+      id: "2",
+      resourceId: "b",
+      start: "2023-06-14T10:00:00",
+      end: "2023-06-14T12:00:00",
+      title: "Event 2",
+    },
+    {
+      id: "3",
+      resourceId: "c",
+      start: "2023-06-14T14:00:00",
+      end: "2023-06-14T16:00:00",
+      title: "Event 3",
+    },
+    {
+      id: "4",
+      resourceId: "d",
+      start: "2023-06-14T14:00:00",
+      end: "2023-06-14T16:00:00",
+      title: "Event 3",
+    },
+    {
+      id: "5",
+      resourceId: "e",
+      start: "2023-06-14T14:00:00",
+      end: "2023-06-14T16:00:00",
+      title: "Event 3",
+    },
+    {
+      id: "6",
+      resourceId: "f",
+      start: "2023-06-14T14:00:00",
+      end: "2023-06-14T16:00:00",
+      title: "Event 3",
+    },
+    {
+      id: "7",
+      resourceId: "g",
+      start: "2023-06-14T14:00:00",
+      end: "2023-06-14T16:00:00",
+      title: "Event 3",
+    },
+  ];
   return (
     <div className="">
-      <div className="text-center">
-        {/* <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList
-              onChange={handleChange}
-              aria-label="lab API tabs example"
-              centered
-            >
-              <Tab label="今日" value="1" />
-              <Tab label="明日" value="2" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">Item One</TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-        </TabContext> */}
-      </div>
-      <div>
-        <Table>
-          <TableHeader className=" bg-gray-300">
-            <TableRow>
-              <TableHead className="w-[100px] ">名前</TableHead>
-              <TableHead className="">開始</TableHead>
-              <TableHead>終了</TableHead>
-              <TableHead className="">イントラ</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium">山田脩太</TableCell>
-              <TableCell>10:00</TableCell>
-              <TableCell>15:00</TableCell>
-              <TableCell className="">美咲</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+      <div className="text-center"></div>
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[resourceTimeGridPlugin]}
+          initialView="resourceTimeGrid"
+          resources={resources}
+          height="70vh"
+          events={events}
+          initialDate="2023-06-14"
+          slotMinTime="09:00:00"
+          slotMaxTime="18:00:00"
+          allDaySlot={false}
+          locale="ja"
+          contentHeight="auto"
+          schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
+        />
       </div>
     </div>
   );
--- src/routes/Departure.tsx
+++ src/routes/Departure.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Footer from "../components/Footer";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
+
 import Button from "../components/Button";
 import DepartureTable from "../components/DepartureTable";
 import {
@@ -17,68 +14,22 @@ import {
   DrawerTrigger,
 } from "../@/components/ui/drawer";
 
-const Departure = () => {
-  const resources = [
-    { id: "a", title: "山田脩太" },
-    { id: "b", title: "𩜙平名春人" },
-    { id: "c", title: "Task C" },
-    { id: "d", title: "Task D" },
-    { id: "e", title: "Task E" },
-    { id: "f", title: "Task F" },
-    { id: "g", title: "Task G" },
-  ];
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../@/components/ui/tabs";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../@/components/ui/table";
 
-  const events = [
-    {
-      id: "1",
-      resourceId: "a",
-      start: "2023-06-14T09:00:00",
-      end: "2023-06-14T11:25:00",
-      title: "Event 1",
-    },
-    {
-      id: "2",
-      resourceId: "b",
-      start: "2023-06-14T10:00:00",
-      end: "2023-06-14T12:00:00",
-      title: "Event 2",
-    },
-    {
-      id: "3",
-      resourceId: "c",
-      start: "2023-06-14T14:00:00",
-      end: "2023-06-14T16:00:00",
-      title: "Event 3",
-    },
-    {
-      id: "4",
-      resourceId: "d",
-      start: "2023-06-14T14:00:00",
-      end: "2023-06-14T16:00:00",
-      title: "Event 3",
-    },
-    {
-      id: "5",
-      resourceId: "e",
-      start: "2023-06-14T14:00:00",
-      end: "2023-06-14T16:00:00",
-      title: "Event 3",
-    },
-    {
-      id: "6",
-      resourceId: "f",
-      start: "2023-06-14T14:00:00",
-      end: "2023-06-14T16:00:00",
-      title: "Event 3",
-    },
-    {
-      id: "7",
-      resourceId: "g",
-      start: "2023-06-14T14:00:00",
-      end: "2023-06-14T16:00:00",
-      title: "Event 3",
-    },
-  ];
+const Departure = () => {
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
@@ -109,21 +60,117 @@ const Departure = () => {
           <DrawerContent>
             <DrawerHeader>
               <DrawerDescription>
-                <div className="demo-app-main">
-                  <FullCalendar
-                    plugins={[resourceTimeGridPlugin]}
-                    initialView="resourceTimeGrid"
-                    resources={resources}
-                    height="70vh"
-                    events={events}
-                    initialDate="2023-06-14"
-                    slotMinTime="09:00:00"
-                    slotMaxTime="18:00:00"
-                    allDaySlot={false}
-                    locale="ja"
-                    contentHeight="auto"
-                    schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
-                  />
+                <div>
+                  <Tabs defaultValue="today">
+                    <TabsList className="w-full">
+                      <TabsTrigger value="today" className="w-[50%]">
+                        今日
+                      </TabsTrigger>
+                      <TabsTrigger value="tomorrow" className="w-[50%]">
+                        明日
+                      </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="today">
+                      <Table>
+                        <TableHeader className=" bg-gray-300">
+                          <TableRow>
+                            <TableHead className="w-[100px] ">名前</TableHead>
+                            <TableHead className="">開始</TableHead>
+                            <TableHead>終了</TableHead>
+                            <TableHead className="">イントラ</TableHead>
+                          </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              山田脩太
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TabsContent>
+                    <TabsContent value="tomorrow">
+                      <Table>
+                        <TableHeader className=" bg-gray-300">
+                          <TableRow>
+                            <TableHead className="w-[100px] ">名前</TableHead>
+                            <TableHead className="">開始</TableHead>
+                            <TableHead>終了</TableHead>
+                            <TableHead className="">イントラ</TableHead>
+                          </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-medium">
+                              副島朝水
+                            </TableCell>
+                            <TableCell>10:00</TableCell>
+                            <TableCell>15:00</TableCell>
+                            <TableCell className="">美咲</TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TabsContent>
+                  </Tabs>
                 </div>
               </DrawerDescription>
             </DrawerHeader>
@@ -138,7 +185,6 @@ const Departure = () => {
           </DrawerContent>
         </Drawer>
       </div>
-      <div></div>
       <Footer />
     </div>
   );
