92788bba9f15545e6e4007c11ef3ff40ddaa02e2
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-09-16 16:35:54 +0900

    fix: エンドポイントを本番環境ように

--- src/api/calendarApi.ts
+++ src/api/calendarApi.ts
@@ -3,14 +3,14 @@ import { Calendar } from "../types/Calendar";
 
 export const getCalendarEvent = async () => {
   const { data } = await axios.get<Calendar[]>(
-    "http://localhost:8000/api/calendar"
+    "https://windap-3ddf402c9faf.herokuapp.com/api/calendar"
   );
   return data;
 };
 
 export const createCalendarEvent = async (values: Calendar) => {
   const { data } = await axios.post<Calendar>(
-    "http://localhost:8000/api/calendar",
+    "https://windap-3ddf402c9faf.herokuapp.com/api/calendar",
     {
       title: values.title,
       content: values.content,
--- src/api/intraClaimApi.ts
+++ src/api/intraClaimApi.ts
@@ -3,28 +3,28 @@ import { IntraClaimType } from "../types/IntraClaim";
 
 export const getIntraClaims = async () => {
   const { data } = await axios.get<IntraClaimType[]>(
-    "http://localhost:8000/api/intraClaims"
+    "https://windap-3ddf402c9faf.herokuapp.com/api/intraClaims"
   );
   return data;
 };
 
 export const showIntraClaim = async (id: number) => {
   const { data } = await axios.get<IntraClaimType>(
-    `http://localhost:8000/api/intraClaim${id}`
+    `https://windap-3ddf402c9faf.herokuapp.com/api/intraClaim${id}`
   );
   return data;
 };
 
 export const IntraApproveClaim = async (id: number) => {
   const { data } = await axios.post(
-    `http://localhost:8000/api/approveClaim/${id}`
+    `https://windap-3ddf402c9faf.herokuapp.com/api/approveClaim/${id}`
   );
   return data;
 };
 
 export const rejectIntraClaim = async (id: number) => {
   const { data } = await axios.post(
-    `http://localhost:8000/api/rejectClaim/${id}`
+    `https://windap-3ddf402c9faf.herokuapp.com/api/rejectClaim/${id}`
   );
   return data;
 };
