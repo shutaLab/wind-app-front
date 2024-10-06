07b68d309b6279a3d8360470d58ba83cc3e92f05
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-10-06 16:52:40 +0900

    fix

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,23 +2,22 @@ import axios from "axios";
 import { User } from "../types/user";
 axios.defaults.withCredentials = true;
 
-axios.defaults.baseURL = "https://windap-api.vercel.app";
 axios.defaults.withCredentials = true; // クッキーを共有するための設定
 
 export const getUser = async () => {
-  const { data } = await axios.get<User>("/api/user");
+  const { data } = await axios.get<User>("https://windap-api.vercel.app/api/user");
   return data;
 };
 
 export const getCsrfCookie = async () => {
-  await axios.get("/sanctum/csrf-cookie");
+  await axios.get("https://windap-api.vercel.app/sanctum/csrf-cookie");
 };
 
 // ログインリクエストを送信する関数
 export const login = async (values: User) => {
   try {
     await getCsrfCookie();
-    const response = await axios.post("/api/login", {
+    const response = await axios.post("https://windap-api.vercel.app/api/login", {
       email: values.email,
       password: values.password,
     });
@@ -30,16 +29,16 @@ export const login = async (values: User) => {
 };
 
 export const signUp = async (values: User) => {
-  const { data } = await axios.post<User>("/api/register", values);
+  const { data } = await axios.post<User>("https://windap-api.vercel.app/api/register", values);
   return data;
 };
 
 export const logout = async () => {
-  const { data } = await axios.post<User>("/api/logout");
+  const { data } = await axios.post<User>("https://windap-api.vercel.app/api/logout");
   return data;
 };
 
 export const notification = async () => {
-  const { data } = await axios.get("/api/notifications");
+  const { data } = await axios.get("https://windap-api.vercel.app/api/notifications");
   return data;
 };
