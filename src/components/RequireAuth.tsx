cfcf80576c9cf37c782b72c09860b8fe1f58fd0c
ing-ShutaYamada <shuta.yamada@in-g.jp>
2024-10-17 12:49:56 +0900

    fix

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -2,16 +2,18 @@ import React, { FC, ReactNode } from 'react'
 import { TailSpin } from 'react-loader-spinner'
 import { useUser } from '../queries/AuthQuery';
 import { Navigate } from 'react-router-dom';
+import { useGetUser } from '../queries/UserQuery';
 type RequireAuthProps = {
   children?: ReactNode;
 };
 export const RequireAuth: FC<RequireAuthProps> = ({ children }) => {
-  const { data: user, isLoading, isError } = useUser();
+  const { data: user, isLoading, isError } = useGetUser();
+  console.log(isLoading)
   console.log(user)
 
-  // if (isLoading) {
-  //   return <TailSpin height="80" width="80" color="#00aab9" />;
-  // }
+  if (isLoading) {
+    return <TailSpin height="80" width="80" color="#00aab9" />;
+  }
 
   if (!user ) {
     return <Navigate to="/login" />;
