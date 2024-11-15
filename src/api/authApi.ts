988339c21f58364b5dea69a2a3aab076f1bcf650
ing-ShutaYamada <shuta.yamada@in-g.jp>
2024-11-15 17:44:50 +0900

    add: 新規登録後自動でログインし遷移

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -12,19 +12,10 @@ export const getCsrfCookie = async () => {
   await apiClient.get(API_ROUTES.AUTH.CSRF);
 };
 
-// ログインリクエストを送信する関数
 export const login = async (values: User) => {
-  try {
-    await getCsrfCookie();
-    const response = await apiClient.post(API_ROUTES.AUTH.LOGIN, {
-      email: values.email,
-      password: values.password,
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Login error:", error);
-    throw error;
-  }
+  await getCsrfCookie();
+  const { data } = await apiClient.post(API_ROUTES.AUTH.LOGIN, values);
+  return data;
 };
 
 export const signUp = async (values: User) => {
