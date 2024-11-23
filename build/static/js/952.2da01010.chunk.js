2608ffe99d3ff3f5e9637b6d09cb80dda18f82a3
ing-ShutaYamada <shuta.yamada@in-g.jp>
2024-11-23 13:48:17 +0900

    add

---
+++ build/static/js/952.2da01010.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwind_front_app=self.webpackChunkwind_front_app||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.2da01010.chunk.js.map
