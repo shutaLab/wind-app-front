6ac5284c6a4894b64aef7993e055d549df1cd182
ing-ShutaYamada <shuta.yamada@in-g.jp>
2024-11-26 15:25:16 +0900

    add

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,13 +4,15 @@ import Footer from "./Footer";
 interface LayoutProps {
   children: ReactNode;
 }
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen bg-gray-200 flex justify-center">
+    <div className="min-h-screen bg-gray-200 flex justify-center relative">
       <div className="w-full max-w-md bg-white min-h-screen shadow-lg flex flex-col">
-        {/* メインコンテンツ */}
-        <main className="flex-1">{children}</main>
-        <Footer />
+        {children}
+        <div className="fixed bottom-0 w-full">
+          <Footer />
+        </div>
       </div>
     </div>
   );
